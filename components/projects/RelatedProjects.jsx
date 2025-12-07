11b2016a54e0b9c605a0d9064f3a34d078dbdbb3
Vijay Kumar <vijay10101992@gmail.com>
2025-12-07 22:17:33 +0530

    realted projects linking to it' details page

--- components/projects/RelatedProjects.jsx
+++ components/projects/RelatedProjects.jsx
@@ -1,26 +1,26 @@
 import Image from 'next/image';
-import { v4 as uuidv4 } from 'uuid';
+import Link from 'next/link';
 
 const RelatedProject = {
 	title: 'Related Projects',
 	Projects: [
 		{
-			id: uuidv4(),
+			id: 4,
 			title: 'Mobile Application: SafeWebKey',
 			img: '/images/safewebkey-app.png',
 		},
 		{
-			id: uuidv4(),
+			id: 6,
 			title: 'Mobile Application: Parking User App',
 			img: '/images/parking-user-app.png',
 		},
 		{
-			id: uuidv4(),
+			id: 2,
 			title: 'Web Application: TheJointCommission',
 			img: '/images/tjc-web-app.png',
 		},
 		{
-			id: uuidv4(),
+			id: 6,
 			title: 'Mobile Application: Partner App',
 			img: '/images/parking-partner-app.png',
 		},
@@ -36,27 +36,32 @@ function RelatedProjects() {
 			</p>
 
 			<div className="grid grid-cols-1 sm:grid-cols-4 gap-10">
-				{RelatedProject.Projects.map((project) => {
+				{RelatedProject.Projects.map((project, index) => {
 					return (
-						<div
-							key={project.id}
-							className="rounded-xl shadow-lg hover:shadow-xl cursor-pointer bg-secondary-light dark:bg-ternary-dark h-full flex flex-col"
+						<Link
+							key={`${project.id}-${index}`}
+							href="/projects/[id]"
+							as={`/projects/${project.id}`}
+							aria-label="Related Project"
+							passHref
 						>
-							<div className="relative w-full h-[432px] overflow-hidden">
-								<Image
-									src={project.img}
-									className="rounded-t-xl border-none object-cover"
-									alt={project.title}
-									layout="fill"
-									objectFit="cover"
-								/>
+							<div className="rounded-xl shadow-lg hover:shadow-xl cursor-pointer bg-secondary-light dark:bg-ternary-dark h-full flex flex-col">
+								<div className="relative w-full h-[432px] overflow-hidden">
+									<Image
+										src={project.img}
+										className="rounded-t-xl border-none object-cover"
+										alt={project.title}
+										layout="fill"
+										objectFit="cover"
+									/>
+								</div>
+								<div className="text-center px-4 py-6 flex-grow flex flex-col justify-center">
+									<p className="font-general-medium text-lg md:text-xl text-ternary-dark dark:text-ternary-light">
+										{project.title}
+									</p>
+								</div>
 							</div>
-							<div className="text-center px-4 py-6 flex-grow flex flex-col justify-center">
-								<p className="font-general-medium text-lg md:text-xl text-ternary-dark dark:text-ternary-light">
-									{project.title}
-								</p>
-							</div>
-						</div>
+						</Link>
 					);
 				})}
 			</div>
