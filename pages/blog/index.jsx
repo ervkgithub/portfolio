b8bf982e274da362eba0aec525ebb1571a15dc1c
Vijay Kumar <vijay10101992@gmail.com>
2026-01-03 09:22:13 +0530

    spacing

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -15,7 +15,7 @@ export default function Blog() {
   });
 
   return (
-    <div className="container mx-auto px-10 py-16">
+    <div className="container mx-auto px-4 py-8 lg:px-10 lg:py-16">
       <h1 className="text-4xl font-bold text-center mb-12 text-gray-900 dark:text-white">My Latest Blog</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
